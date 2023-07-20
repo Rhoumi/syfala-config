dc6627f4b8131e9d050c9b6a2751b896ed691e74
Raphael Forment <raphael.forment@gmail.com>
2023-07-20 16:27:49 +0200

    lol

--- syfala_tui/__main__.py
+++ syfala_tui/__main__.py
@@ -1,8 +1,9 @@
 from pprint import pprint as pretty_print
 from rich.panel import Panel
+from typing import Optional
 from platform import system
+from .utils import _banner 
 from pathlib import Path
-from .utils import funny_text
 import inquirer
 
 POSSIBLE_KEYS = {
@@ -61,24 +62,50 @@ POSSIBLE_KEYS = {
 
 class Tui:
 
-    def __init__(self, config_file_path: Path):
-        print(funny_text)
-        self.config_path = config_file_path
+    def __init__(self, config: Path):
+        print(_banner)
+        self.config_path = config
+        self.config_file: Optional[dict] = None
+        try:
+            self.config_file: dict = self._read_config_file(self.config_path)
+        except FileNotFoundError or Exception as e:
+            self.config_file: dict = self._create_template_makefile_env()
 
-    def create_template_makefile_env(self, config_file_path: Path|str) -> None:
-        """If the makefile env file is not found, create a template"""
 
-        if config_file_path.exists():
+    def _create_template_makefile_env(self, config_file_path: Path|str) -> None:
+        """If the makefile.env file does not exist, create a template file.
+        The template file will be created in the default syfala-project 
+        directory. If the directory does not exist, it will be created.
+
+        Args:
+            config_file_path (Path | str): Path to the makefile.env file
+        """
+        if config_file_path.exists() and config_file_path.is_file():
             return
         else:
+            print('====> Creating makefile.env file (and possibly syfala-project directory)')
             try:
-                config_file_path.touch()
+                if not config_file_path.parent.exists():
+                    config_file_path.parent.mkdir(parents=True)
+                    config_file_path.touch()
+                else:
+                    config_file_path.touch()
             except Exception as e:
-                print(f"Could not create template file: {str(e)}")
+                raise Exception(f"Could not create makefile.env file: {str(e)}")
+        return self._parse_makefile_env(config_file_path)
 
-    def parse_makefile_env(self, file_path: Path|str) -> dict:
+    def _parse_makefile_env(self, file_path: Path|str) -> dict:
+        """Parse the makefile.env file and return a dictionary with the
+        variables and their values. If the file does not exist, return
+        an empty dictionary.
+
+        Args:
+            file_path (Path | str): Path to the makefile.env file
+
+        Returns:
+            dict: Dictionary containing the config variables and their values
+        """
         variables = {} | POSSIBLE_KEYS
-        print(f"Reading configuration file: {file_path}")
         try:
             with open(file_path, 'r') as file:
                 for line in file:
@@ -89,25 +116,36 @@ class Tui:
                             if var.strip() in POSSIBLE_KEYS.keys():
                                 variables[var.strip()] = value.strip()
                             else:
-                                raise KeyError(f"Unknown variable: {var.strip()}")
+                                print(f'====> Unknown variable in config file: {var.strip()}')
         except FileNotFoundError:
-            print("Could not find makefile.env file!")
+            variables = self._create_template_makefile_env(file_path)
+
         # Return dictionary whose values are not None
         return {key: value for key, value in variables.items() if value is not None}
 
-    def read_config_file(self, makefile_env_path) -> dict:
-        try:
-            variables = self.parse_makefile_env(makefile_env_path)
-        except Exception as e:
-            raise Exception(f"Could not parse makefile.env file: {str(e)}")
-        return variables
+    def _read_config_file(self, makefile_env_path) -> dict:
+        """Entry point for reading the makefile.env file. This function
+        will try to parse the makefile.env file and return a dictionary
+
+        Args:
+            makefile_env_path (_type_): Path to the makefile.env file
+
+        Raises:
+            Exception: Exception raised if the makefile.env file 
+            could not be parsed. Note that the program will try
+            to create a template makefile.env file if it does not
+            exist or is invalid.
+
+        Returns:
+            dict: Dictionary containing the syfala config variables and their values 
+        """
+        return self._parse_makefile_env(makefile_env_path)
 
     def run(self):
-        try:
-            CONFIG_FILE: dict = self.read_config_file(PROJECT_DIR)
-        except FileNotFoundError or Exception as e:
-            CONFIG_FILE: dict = self.create_template_makefile_env()
-        pretty_print(CONFIG_FILE)
+        pretty_print(self.config_file)
+
+    def main_menu(self):
+        ...
 
 if __name__ == "__main__":
     # Path for the syfala user configuration file
