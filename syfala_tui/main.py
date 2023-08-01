cba44c238448f9731898c3a60297a32db11c1bcf
Ralt144MI <75539795+Rhoumi@users.noreply.github.com>
2023-08-01 13:15:31 +0200

    Delete main.py

--- syfala_tui/main.py
+++
@@ -1,8 +0,0 @@
-"""
-This is a legacy entrypoint to retain ``sardine.cli.main``. The recommended method
-to run this module is to use ``python -m sardine.cli`` or ``sardine-config``.
-"""
-from sardine_core.cli import *
-
-if __name__ == "__main__":
-    main()
